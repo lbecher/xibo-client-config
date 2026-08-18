7d9f0d91c6506d43a69be86064737a01c85703c7
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2026-08-18 15:40:04 -0300

    atualizando script de instalação e configuração do sway para suportar novas versões do Xibo

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 
 #---------------------------------------
 # Identifica a arquitetura do sistema e seta variáveis
-BASE_URL="https://github.com/lbecher/xibo-client-config/releases/download/v0.2.0"
-ARCH=$(uname -m)
+XIBO_VERSION="${XIBO_VERSION:-4.0.9}"
+BASE_URL="https://github.com/lbecher/xibo-client-config/releases/download/v${XIBO_VERSION}"
+ARCH=$(dpkg --print-architecture)
 case "$ARCH" in
-    "x86_64")
-        FILE="xibo-player_1.8-R7_amd64.snap"
-        ;;
-    "armv7l")
-        FILE="xibo-player_1.8-R7_armhf.snap"
-        ;;
-    "aarch64")
-        FILE="xibo-player_1.8-R7_arm64.snap"
+    "amd64"|"arm64")
+        FILE="xibo-player_${XIBO_VERSION}_${ARCH}.deb"
         ;;
     *)
         echo "Arquitetura não suportada: $ARCH"
@@ -37,7 +32,6 @@ sudo apt install \
     sway \
     rofi \
     alacritty \
-    snapd \
     at-spi2-core \
     gvfs \
     gvfs-backends \
@@ -63,5 +57,7 @@ cp sway_config ~/.config/sway/config
 
 #---------------------------------------
 # Instalando dependências
-wget "$BASE_URL/$FILE"
-sudo snap install --dangerous "$FILE"
+DEB_PATH=$(mktemp --suffix=.deb)
+trap 'rm -f "$DEB_PATH"' EXIT
+wget --output-document="$DEB_PATH" "$BASE_URL/$FILE"
+sudo apt install -y "$DEB_PATH"
